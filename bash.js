ed633ccf2bebdc3d30b0ae1353fb973f4445374a
CJ <cliftonjones.87@gmail.com>
2021-09-24 15:48:44 -0400

    made changes

--- bash.js
+++ bash.js
@@ -1,13 +1,23 @@
+const ls = require("./ls");
+const pwd = require("./pwd");
+const cat = require("./cat");
+
 // Output a prompt
-process.stdout.write('prompt > ');
+process.stdout.write("prompt > ");
 
 // The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', (data) => {
+process.stdin.on("data", (data) => {
   const cmd = data.toString().trim(); // remove the newline
 
-  if (cmd === 'pwd') {
-    process.stdout.write(process.cwd());
+  if (cmd === "pwd") {
+    pwd();
   }
 
-  process.stdout.write('\nprompt > ');
+  if (cmd === "ls") {
+    ls();
+  }
+
+  if (cmd.split(" ")[0] === "cat") {
+    cat(cmd.split(" ")[1]);
+  }
 });
